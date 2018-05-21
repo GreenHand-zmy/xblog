38707468e821f3b45d62af2eb880933b5b5229c6
zmy <573093826@qq.com>
2018-05-21 17:07:12 +0800

    添加表名常量

---
+++ src/main/java/constant/TableNameConstant.java
@@ -0,0 +1,27 @@
+package constant;
+
+/**
+ * 表名常量
+ * Created by zmy on 2018/5/21.
+ */
+public interface TableNameConstant {
+    /**
+     * 频道表名
+     */
+    String CHANNEL_TABLE = "mto_channels";
+
+    /**
+     * 评论表名
+     */
+    String COMMENT_TABLE = "mto_comments";
+
+    /**
+     * 文章表名
+     */
+    String POST_TABLE = "mto_posts";
+
+    /**
+     * 用户表名
+     */
+    String USER_TABLE = "mto_users";
+}
