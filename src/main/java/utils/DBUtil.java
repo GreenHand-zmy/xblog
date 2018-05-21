46ed42b39ec4d52f634cd60e269d70eb19b1ade8
zmy <573093826@qq.com>
2018-05-21 16:48:31 +0800

    修改dbUti bug

--- src/main/java/utils/DBUtil.java
+++ src/main/java/utils/DBUtil.java
@@ -15,62 +15,63 @@ import java.util.List;
  * Created by ZH on 2018/5/21.
  */
 public class DBUtil {
-    public DataSource getDateSource(){
-        DataSource ds=new ComboPooledDataSource();
-        return ds;
+    private static DataSource dataSource = new ComboPooledDataSource();
+
+    public DataSource getDateSource() {
+        return dataSource;
     }
 
-    public Connection getConn(){
-        Connection conn=null;
+    public Connection getConn() {
+        Connection conn = null;
         try {
-            conn=getDateSource().getConnection();
+            conn = getDateSource().getConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return conn;
     }
 
-    public int executeUpdate(String sql,Object...params){
-        int num=0;
-        Connection conn=getConn();
-        QueryRunner runner=new QueryRunner();
-        try {
-            num=runner.update(conn,sql,params);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return num;
-    }
-
-    public int getCoount(String sql,Class clazz,Object...params){
-        int num=0;
-        Connection conn=getConn();
-        QueryRunner runner=new QueryRunner();
-        try {
-            Object obj=runner.query(conn,sql,new ScalarHandler(),params);
-            num= Integer.parseInt(obj.toString());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return num;
-    }
-
-    public <T> T getObject(String sql,Class clazz,Object...params){
-        Connection conn=getConn();
+    public int executeUpdate(String sql, Object... params) {
+        int num = 0;
+        Connection conn = getConn();
         QueryRunner runner = new QueryRunner();
         try {
-            return (T) runner.query(conn,sql,new BeanHandler<Class>(clazz),params);
+            num = runner.update(conn, sql, params);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return num;
+    }
+
+    public int getCoount(String sql, Class clazz, Object... params) {
+        int num = 0;
+        Connection conn = getConn();
+        QueryRunner runner = new QueryRunner();
+        try {
+            Object obj = runner.query(conn, sql, new ScalarHandler(), params);
+            num = Integer.parseInt(obj.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return num;
+    }
+
+    public <T> T getObject(String sql, Class clazz, Object... params) {
+        Connection conn = getConn();
+        QueryRunner runner = new QueryRunner();
+        try {
+            return (T) runner.query(conn, sql, new BeanHandler<Class>(clazz), params);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public List getObjects(String sql, Class clazz, Object...params){
-        Connection conn=getConn();
+    public List getObjects(String sql, Class clazz, Object... params) {
+        Connection conn = getConn();
         QueryRunner runner = new QueryRunner();
         try {
-            return runner.query(conn,sql,new BeanListHandler<Class>(clazz),params);
+            return runner.query(conn, sql, new BeanListHandler<Class>(clazz), params);
         } catch (SQLException e) {
             e.printStackTrace();
         }
