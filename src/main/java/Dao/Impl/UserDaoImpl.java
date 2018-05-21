1e8d25401491d9ab8610bc7b80b2f623d5e8ebb7
zmy <573093826@qq.com>
2018-05-21 16:56:38 +0800

    bug fix

--- src/main/java/Dao/Impl/UserDaoImpl.java
+++ src/main/java/Dao/Impl/UserDaoImpl.java
@@ -9,40 +9,40 @@ import java.util.List;
 /**
  * Created by ZH on 2018/5/21.
  */
-public class UserDaoImpl extends DBUtil implements UserDao {
+public class UserDaoImpl implements UserDao {
     @Override
     public int addUser(User user) {
-        String sql="insert into mto_users(created,password,username,name) values(sysDate(),?,?,?)";
-        return super.executeUpdate(sql,user.getPassword(),user.getUsername(),user.getName());
+        String sql = "insert into mto_users(created,password,username,name) values(sysDate(),?,?,?)";
+        return DBUtil.executeUpdate(sql, user.getPassword(), user.getUsername(), user.getName());
     }
 
     @Override
     public int delUser(int id) {
-        String sql="delete from mto_users where id=?";
-        return super.executeUpdate(sql,id);
+        String sql = "delete from mto_users where id=?";
+        return DBUtil.executeUpdate(sql, id);
     }
 
     @Override
     public int updateUser(User user) {
-        String sql="update mto_users set password=?,username=?,name=?,avatar=?,signature=?,updated=sysDate() where id=?";
-        return super.executeUpdate(sql,user.getPassword(),user.getUsername(),user.getName(),user.getAvatar(),user.getSignature(),user.getId());
+        String sql = "update mto_users set password=?,username=?,name=?,avatar=?,signature=?,updated=sysDate() where id=?";
+        return DBUtil.executeUpdate(sql, user.getPassword(), user.getUsername(), user.getName(), user.getAvatar(), user.getSignature(), user.getId());
     }
 
     @Override
     public User getUser(int id) {
-        String sql="select * from mto_users where id=?";
-        return super.getObject(sql,User.class,id);
+        String sql = "select * from mto_users where id=?";
+        return DBUtil.getObject(sql, User.class, id);
     }
 
     @Override
     public List<User> getAll() {
-        String sql="select * from mto_users";
-        return super.getObjects(sql,User.class);
+        String sql = "select * from mto_users";
+        return DBUtil.getObjects(sql, User.class);
     }
 
     @Override
     public int isExits(String username) {
-        String sql="select count(*) from mto_users where username=?";
-        return super.getCoount(sql,User.class,username);
+        String sql = "select count(*) from mto_users where username=?";
+        return DBUtil.getCoount(sql, User.class, username);
     }
 }
