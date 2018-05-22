41a0259a0271bcae3ac63233a4c80030c8fc1555
hualuomg <979672450@qq.com>
2018-05-22 16:42:27 +0800

    Posts

--- src/main/java/controller/PostsServlet.java
+++ src/main/java/controller/PostsServlet.java
@@ -1,5 +1,9 @@
 package controller;
 
+import Service.Impl.PostsServiceImpl;
+import Service.PostsService;
+import bean.Posts;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,12 +11,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by Fang on 2018/5/22.
  */
 @WebServlet("/PostsServlet")
 public class PostsServlet extends HttpServlet{
+    PostsService ps=new PostsServiceImpl();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
@@ -23,6 +32,14 @@ public class PostsServlet extends HttpServlet{
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html;utf-8");
         PrintWriter out = resp.getWriter();
-
+        long id=Integer.parseInt(req.getParameter("id"));
+        long authorId=Integer.parseInt(req.getParameter("authorId"));
+        String op=req.getParameter("op");
+        List<Posts> postsList =new ArrayList<Posts>();
+        if("selectAuthorId".equals(op)){
+            postsList=ps.getPostAuthorId(authorId);
+        }
+        req.setAttribute("postsList",postsList);
+        req.getRequestDispatcher("").forward(req,resp);
     }
 }
