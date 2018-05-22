99b7794ad1c8c406c280a739b6d133a41428b9a3
hualuomg <979672450@qq.com>
2018-05-22 09:04:52 +0800

    文章Dao与Impl

--- src/main/java/Dao/Impl/PostDaoImpl.java
+++ src/main/java/Dao/Impl/PostDaoImpl.java
@@ -2,7 +2,6 @@ package Dao.Impl;
 
 import Dao.PostDao;
 import bean.Posts;
-import bean.User;
 import utils.DBUtil;
 
 import java.util.List;
@@ -10,14 +9,13 @@ import java.util.List;
 /**
  * Created by Fang on 2018/5/21.
  */
-public class PostDaoImpl extends DBUtil implements PostDao {
+public class PostDaoImpl  implements PostDao {
 
-     DBUtil db =new DBUtil();
     @Override
     public int addPost(Posts post) {
         String sql="insert into mto_posts (author_id,comments,created,editor,favors,featured,channel_id,status,summary,tags,title,views,weight) " +
                 "values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
-        return super.executeUpdate(sql,post.getAuthorId(),post.getComments(),
+        return DBUtil.executeUpdate(sql,post.getAuthorId(),post.getComments(),
                 post.getCreated(), post.getEditor(),post.getFavors(),post.getFeatured(),
                 post.getChannelId(), post.getStatus(),post.getSummary(),post.getTags(),post.getTitle(),
                 post.getViews(),post.getWeight());
@@ -27,36 +25,36 @@ public class PostDaoImpl extends DBUtil implements PostDao {
     public int updatePost(Posts post) {
         String sql="update mto_posts set channel_id=?,tags=?,title=? where id=?";
         Object[] values={post.getChannelId(),post.getTags(),post.getTitle(),post.getId()};
-        return super.executeUpdate(sql,values);
+        return DBUtil.executeUpdate(sql,values);
     }//修改文章
 
     @Override
     public int deletePost(Long Id) {
         String sql="delete from mto_posts where id=?";
-        return super.executeUpdate(sql, Id);
+        return DBUtil.executeUpdate(sql, Id);
     }//删除文章
 
     @Override
     public List<Posts> getPostAuthorId(Long authorId) {
         String sql="select * from mto_posts where author_id = ?";
-        return db.getObjects(sql,Posts.class,authorId);
+        return DBUtil.getObjects(sql,Posts.class,authorId);
     }//根据作者编号查询
 
     @Override
     public List<Posts> getPostTitle(String title) {
         String sql="select * from mto_posts where title like '%"+title+"%'";
-        return db.getObjects(sql,Posts.class);
+        return DBUtil.getObjects(sql,Posts.class);
     }//根据文章标题模糊查询
 
     @Override
     public List<Posts> getAllPosts() {
         String sql="select * from mto_posts";
-        return db.getObjects(sql,Posts.class);
+        return DBUtil.getObjects(sql,Posts.class);
     }//查询所有
     @Override
     public int isExits(Long id) {
         String sql = "select count(*) from mto_posts where id=?";
-        return DBUtil.getCoount(sql, Posts.class, id);
+        return DBUtil.getCount(sql, Posts.class, id);
     }//查询id是否存在
     public Posts getPost(Long id) {
         String sql = "select * from mto_posts where id=?";
