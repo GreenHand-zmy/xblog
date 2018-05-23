811cad83c2c5b31dcc4411951bbe75cdc6a0db86
zmy <573093826@qq.com>
2018-05-23 09:56:11 +0800

    解决中文乱码问题

---
+++ src/main/java/filter/EncodingFilter.java
@@ -0,0 +1,34 @@
+package filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by zmy on 2018/5/23.
+ */
+@WebFilter(value = {"/"})
+public class EncodingFilter implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest)request;
+        HttpServletResponse resp = (HttpServletResponse)response;
+
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+
+        chain.doFilter(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
