d64bbb59661f45d6753377ccb3092777a3a85fe1
hualuomg <979672450@qq.com>
2018-05-23 15:24:40 +0800

    Posts

--- src/main/java/controller/PostsServlet.java
+++ src/main/java/controller/PostsServlet.java
@@ -1,7 +1,12 @@
 package controller;
 
+import Dao.Impl.PostDaoImpl;
+import Dao.PostDao;
+import Service.ChannelService;
+import Service.Impl.ChannelServiceImpl;
 import Service.Impl.PostsServiceImpl;
 import Service.PostsService;
+import bean.Channel;
 import bean.Posts;
 import bean.User;
 
@@ -18,60 +23,16 @@ import java.util.*;
  * Created by Fang on 2018/5/22.
  */
 @WebServlet("/PostsServlet")
-public class PostsServlet extends HttpServlet{
-    private PostsService ps=new PostsServiceImpl();
+public class PostsServlet extends HttpServlet {
+    private PostsService ps = new PostsServiceImpl();
     private PostsService postsService = new PostsServiceImpl();
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
+    private ChannelService channelService = new ChannelServiceImpl();
+    private PostDao postDao = new PostDaoImpl();
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html;utf-8");
-        PrintWriter out = resp.getWriter();
-        long id=Integer.parseInt(req.getParameter("id"));
-        long authorId=Integer.parseInt(req.getParameter("authorId"));
-        String op=req.getParameter("op");
-        List<Posts> postsList =new ArrayList<Posts>();
-        int LIMIT = 5;
-        if("selectAuthorId".equals(op)){
-            postsList=ps.getPostAuthorId(authorId);
-        }else if("selectAll".equals(op)){
-            postsList=ps.getAllPosts();
-        }else if("selectByTime".equals(op)){
-            postsList=ps.findNewPostsLimit(LIMIT);
-        }else if("selectByFavors".equals(op)){
-            postsList=ps.findNewPostsLimit2(LIMIT);
-        }else if("selectByComments".equals(op)){
-            postsList=ps.findNewPostsLimit3(LIMIT);
-        }else if("updatePost".equals(op)){
-            updatePost(req, resp);
-        }else if("updatePostFavors".equals(op)){
-            updatePostFavors(req, resp);
-        }else if("updatePostViews".equals(op)){
-            updatePostViews(req, resp);
-        }else if("updatePostComments".equals(op)){
-            updatePostComments(req, resp);
-        }else if("addPost".equals(op)){
-            addPost(req, resp);
-        }else if("getPost".equals(op)){
-            getPost(req,resp);
-        }else if("delPost".equals(op)){
-            postsService.deletePost(id);
-        }
-        req.setAttribute("postsList",postsList);
-        req.getRequestDispatcher("").forward(req,resp);
-    }
-
-    private void delPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-    }
-
-    private void getPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+    private void getPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Posts post = new Posts();
         PrintWriter out = resp.getWriter();
-        long id=Integer.parseInt(req.getParameter("id"));
+        long id = Integer.parseInt(req.getParameter("id"));
         post = postsService.getPost(id);
         resp.sendRedirect("jsps/default/auth/view.jsp");
     }
@@ -79,8 +40,9 @@ public class PostsServlet extends HttpServlet{
     private void addPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Posts post = new Posts();
         Date created = new Date();
+        List<Channel> channelList = channelService.findAll();
         User user = (User) req.getSession().getAttribute("user");
-        int channelId = Integer.parseInt(req.getParameter("channelId"));;
+        int channelId = Integer.parseInt(req.getParameter("channelId"));
         String tags = req.getParameter("tags");
         String title = req.getParameter("title");
         String summary = req.getParameter("summary");
@@ -97,42 +59,48 @@ public class PostsServlet extends HttpServlet{
         post.setViews(0);
         post.setWeight(0);
         postsService.addPost(post);
-        resp.sendRedirect("jsps/default/auth/index.jsp");
+        if(postDao.addPost(post)>0){
+            resp.sendRedirect("jsps/default/index.jsp");
+        }
     }
+
     private void updatePostFavors(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Posts post = new Posts();
         PrintWriter out = resp.getWriter();
-        long id=Integer.parseInt(req.getParameter("id"));
+        long id = Integer.parseInt(req.getParameter("id"));
         post = postsService.getPost(id);
-        int favors=post.getFavors()+1;
+        int favors = post.getFavors() + 1;
         post.setFeatured(favors);
         postsService.updatePost(post);
-        resp.sendRedirect("jsps/default/auth/view.jsp?id="+post.getId());
+        resp.sendRedirect("jsps/default/auth/view.jsp?id=" + post.getId());
     }
+
     private void updatePostViews(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Posts post = new Posts();
         PrintWriter out = resp.getWriter();
-        long id=Integer.parseInt(req.getParameter("id"));
+        long id = Integer.parseInt(req.getParameter("id"));
         post = postsService.getPost(id);
-        int views=post.getViews()+1;
+        int views = post.getViews() + 1;
         post.setViews(views);
         postsService.updatePost(post);
-        resp.sendRedirect("jsps/default/auth/view.jsp?id="+post.getId());
+        resp.sendRedirect("jsps/default/auth/view.jsp?id=" + post.getId());
     }
+
     private void updatePostComments(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Posts post = new Posts();
         PrintWriter out = resp.getWriter();
-        long id=Integer.parseInt(req.getParameter("id"));
+        long id = Integer.parseInt(req.getParameter("id"));
         post = postsService.getPost(id);
-        int comments=post.getComments()+1;
+        int comments = post.getComments() + 1;
         post.setComments(comments);
         postsService.updatePost(post);
-        resp.sendRedirect("jsps/default/auth/view.jsp?id="+post.getId());
+        resp.sendRedirect("jsps/default/auth/view.jsp?id=" + post.getId());
     }
+
     private void updatePost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Posts post = new Posts();
         PrintWriter out = resp.getWriter();
-        long id=Integer.parseInt(req.getParameter("id"));
+        long id = Integer.parseInt(req.getParameter("id"));
         post = postsService.getPost(id);
         int channelId = Integer.parseInt(req.getParameter("channelId"));
         String tags = req.getParameter("tags");
@@ -150,5 +118,43 @@ public class PostsServlet extends HttpServlet{
         post.setStatus(status);
         postsService.updatePost(post);
         resp.sendRedirect("jsps/default/auth/index.jsp");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String op = req.getParameter("op");
+        List<Posts> postsList = new ArrayList<Posts>();
+        int LIMIT = 5;
+        if ("selectAuthorId".equals(op)) {
+            PrintWriter out = resp.getWriter();
+            long authorId = Integer.parseInt(req.getParameter("authorId"));
+            postsList = ps.getPostAuthorId(authorId);
+        } else if ("selectAll".equals(op)) {
+            postsList = ps.getAllPosts();
+        } else if ("selectByTime".equals(op)) {
+            postsList = ps.findNewPostsLimit(LIMIT);
+        } else if ("selectByFavors".equals(op)) {
+            postsList = ps.findNewPostsLimit2(LIMIT);
+        } else if ("selectByComments".equals(op)) {
+            postsList = ps.findNewPostsLimit3(LIMIT);
+        } else if ("updatePost".equals(op)) {
+            updatePost(req, resp);
+        } else if ("updatePostFavors".equals(op)) {
+            updatePostFavors(req, resp);
+        } else if ("updatePostViews".equals(op)) {
+            updatePostViews(req, resp);
+        } else if ("updatePostComments".equals(op)) {
+            updatePostComments(req, resp);
+        } else if ("toAddPostPage".equals(op)) {
+            req.getRequestDispatcher("jsps/default/channel/editing.jsp").forward(req, resp);
+        } else if ("addPost".equals(op)) {
+            addPost(req, resp);
+        } else if ("getPost".equals(op)) {
+            getPost(req, resp);
+        } else if ("delPost".equals(op)) {
+            PrintWriter out = resp.getWriter();
+            long id = Integer.parseInt(req.getParameter("id"));
+            postsService.deletePost(id);
+        }
     }
 }
