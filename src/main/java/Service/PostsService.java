6920d924f35225509fb295706d23f7e9cee6e071
zmy <573093826@qq.com>
2018-05-24 14:24:39 +0800

    添加注释以及增加api

--- src/main/java/Service/PostsService.java
+++ src/main/java/Service/PostsService.java
@@ -10,19 +10,40 @@ import java.util.List;
  */
 public interface PostsService {
     public int addPost(Posts post);//添加文章
+
     public int updatePost(Posts post);//修改文章
+
     public int deletePost(Long Id);//删除文章
+
     public int updatePostFavors(Posts post);//修改文章点赞数
+
     public int updatePostViews(Posts post);//修改文章阅读量
+
     public int updatePostComments(Posts post);//修改文章评论量
+
     public List<Posts> getPostTitle(String title);//根据文章标题模糊查询
+
     public List<Posts> getPostAuthorId(Long authorId);//根据作者标号查询
+
     public List<Posts> getChannelPosts(Long channel);//根据频道id查询所有文章
+
     public List<Posts> getAllPosts();//查询所有
+
     public Posts getPost(Long id);//根据id查询文章
+
     public List<Posts> findNewPostsLimit(int LIMIT);//根据时间查询前LIMIT条
+
     public List<Posts> findNewPostsLimit2(int LIMIT);//根据点赞查询前LIMIT条
+
     public List<Posts> findNewPostsLimit3(int LIMIT);//根据评论查询前LIMIT条
     public List<Posts> findNewPostsLimit4(int LIMIT);//根据评论查询前LIMIT条
-    PageBean<Posts> findByPage(int pageIndex,int limit);
+
+    /**
+     * 获取分页数据
+     *
+     * @param pageIndex 页码
+     * @param pageSize  页大小
+     * @return
+     */
+    PageBean<Posts> findByPage(int pageIndex, int pageSize);
 }
--- src/test/java/com/peter/xblog/dao/PostsDaoTest.java
+++ src/test/java/com/peter/xblog/dao/PostsDaoTest.java
@@ -13,111 +13,124 @@ import java.util.List;
  */
 public class PostsDaoTest {
     PostDao pd = new PostDaoImpl();
+
     //测试添加文章
     @Test
     public void testAdd() {
         Date date = new Date();
-        Posts post = new Posts(2L, 3, date, "4", 5, 1, 1, 1, "1", "1", "1", 1,2,"321");
+        Posts post = new Posts(2L, 3, date, "4", 5, 1, 1, 1, "1", "1", "1", 1, 2, "321");
         int num = pd.addPost(post);
         System.out.println(num);
     }
+
     //测试按作者id查找文章
     @Test
     public void testSelect() {
         List<Posts> list = pd.getPostAuthorId(2L);
-        for (Posts post :list) {
+        for (Posts post : list) {
             System.out.println(post);
         }
     }
+
     //测试按栏目id查找文章
     @Test
     public void testSelect2() {
         List<Posts> list = pd.getChannelPosts(1L);
-        for (Posts post :list) {
+        for (Posts post : list) {
             System.out.println(post);
         }
     }
+
     //测试按文章标题模糊查询
     @Test
     public void testSelect3() {
         List<Posts> list = pd.getPostTitle("123");
-        for (Posts post :list) {
+        for (Posts post : list) {
             System.out.println(post);
         }
     }
+
     //测试删除文章
     @Test
     public void testDelect() {
         pd.deletePost(8L);
         List<Posts> list = pd.getAllPosts();
-        for (Posts post :list) {
+        for (Posts post : list) {
             System.out.println(post);
         }
     }
+
     //测试查找所有文章
     @Test
     public void testSelectAll() {
         List<Posts> list = pd.getAllPosts();
-        for (Posts post :list) {
+        for (Posts post : list) {
             System.out.println(post);
         }
     }
+
     //测试更改文章
     @Test
     public void testUpdate() {
-        Posts post = new Posts(7,2,2,2,"2",2,"234");
+        Posts post = new Posts(7, 2, 2, 2, "2", 2, "234");
         pd.updatePost(post);
         List<Posts> list = pd.getPostAuthorId(2L);
-        for (Posts post1 :list) {
+        for (Posts post1 : list) {
             System.out.println(post);
         }
     }
+
     //测试更改文章点赞数
     @Test
     public void testUpdate2() {
-        Posts post = new Posts(7,2);
+        Posts post = new Posts(7, 2);
         pd.updatePostFavors(post);
         List<Posts> list = pd.getPostAuthorId(2L);
-        for (Posts post1 :list) {
+        for (Posts post1 : list) {
             System.out.println(post);
         }
     }
+
     //测试更改文章阅读数
     @Test
     public void testUpdate3() {
-        Posts post = new Posts(7,2);
+        Posts post = new Posts(7, 2);
         pd.updatePostViews(post);
         List<Posts> list = pd.getPostAuthorId(2L);
-        for (Posts post1 :list) {
+        for (Posts post1 : list) {
             System.out.println(post);
         }
     }
+
     //测试更改文章评论数
     @Test
     public void testUpdate4() {
-        Posts post = new Posts(7,3);
+        Posts post = new Posts(7, 3);
         pd.updatePostComments(post);
         List<Posts> list = pd.getPostAuthorId(2L);
-        for (Posts post1 :list) {
+        for (Posts post1 : list) {
             System.out.println(post);
         }
     }
+
     //测试根据时间顺序查找文章
     @Test
     public void testSelectAll2() {
         List<Posts> list = pd.findNewPostsLimit(1);
-        for (Posts post :list) {
+        for (Posts post : list) {
             System.out.println(post);
         }
     }
+
     //测试根据点赞数顺序查找文章
     @Test
     public void testSelectAll3() {
         List<Posts> list = pd.findNewPostsLimit2(3);
-        for (Posts post :list) {
+        for (Posts post : list) {
             System.out.println(post);
         }
     }
+
     //测试根据评论数顺序查找文章
     @Test
     public void testSelectAll4() {
@@ -125,5 +138,16 @@ public class PostsDaoTest {
         for (Posts post : list) {
             System.out.println(post);
         }
+    }
+
+    @Test
+    public void countAll() {
+        Integer integer = pd.countAll();
+        System.out.println(integer);
+    }
+
+    @Test
+    public void findByOffsetAndLimit() {
+        List<Posts> postsList = pd.findByOffsetAndLimit(0, 3);
     }
 }
