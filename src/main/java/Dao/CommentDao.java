7e94d2952f35b2f322ff17e626701032617e1a60
lfy <296668139@qq.com>
2018-05-24 09:50:16 +0800

    注释

--- src/main/java/Dao/CommentDao.java
+++ src/main/java/Dao/CommentDao.java
@@ -9,9 +9,9 @@ import java.util.List;
  */
 public interface CommentDao {
     public Comment getCommentById(long id );//根据评论编号得到评论
-    public List<Comment> getComments(long toid);//根据所评论的文章（toid）得到该文章的所有评论
+    public List<Comment> getComments(long toid);//根据所评论的文章toid（对应于当前文章id）得到该文章的所有评论
     public List<Comment> getCommentsByAuthor(long authorId);//得到一个用户的所有评论
     public int delComment(long id);//根据评论编号删除评论
     public int addComment(Comment comment);//增加一条评论
-    public int getCount(long toid);//根据所评论文章得到该文章的评论数
+    public int getCount(long toid);//根据所评论文章（对应于当前文章id）得到该文章的评论数
 }
--- src/test/java/com/peter/xblog/service/CommentServiceTest.java
+++ src/test/java/com/peter/xblog/service/CommentServiceTest.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class CommentServiceTest {
     CommentService commentService = new CommentServiceImpl();
-
+    //测试添加评论
     @Test
     public void testAdd() {
         Date date = new Date();
@@ -22,19 +22,19 @@ public class CommentServiceTest {
         int num = commentService.addComment(comment);
         System.out.println(num);
     }
-
+    //测试数据库连接
     @Test
     public void testgetCount() {
         System.out.println(commentService.getCount(3));
     }
-
+    //测试根据所评论文章toId(对应于当前文章的id)得到该文章的评论数
     @Test
     public void testgetCommentById() {
         long id = 1;
         Comment comment = commentService.getCommentById(id);
         System.out.println(comment.getContent());
     }
-
+    //测试根据id查询评论
     @Test
     public void getComments() {
         long toid = 3;
@@ -43,7 +43,7 @@ public class CommentServiceTest {
             System.out.println(c.getContent());
         }
     }
-
+    //测试根据所评论的文章toid（对应于当前文章的Id）得到该文章的所有评论
     @Test
     public void testgetCommentsByAuthor() {
         long authorid = 2;
@@ -52,13 +52,13 @@ public class CommentServiceTest {
             System.out.println(c.getContent());
         }
     }
-
+    //测试根据用户Id查询评论
     @Test
     public void testDel() {
         int num = commentService.delComment(1);
         System.out.println(num);
     }
-
+    //测试删除评论
     @Test
     public void getPostCommentVoByAuthorId() {
         List<PostCommentVo> postCommentVoByAuthorId = commentService.getPostCommentVoByAuthorId(6L);
